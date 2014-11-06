e98f6559cd80d4953c6a81aaa1f11578b732b0d5
Christian Long <christianzlong2@gmail.com>
2014-11-06 16:27:42 -0600

    A working Pelican blog.

--- content/first_post.md
+++ content/first_post.md
@@ -1,4 +1,4 @@
-Title: First Post
+Title: Pelican 
 Category: Python
 Tags: pelican, blog
 Slug: first-post
@@ -7,5 +7,22 @@ Summary: First post to my new Pelican-Powered blog
 
 With a bit of wrangling, I got Pelican working to manage my blog. [Pelican](http://blog.getpelican.com/) is a static site generator written in Python. It can handle reStructuredText, Markdown, or AsciiDoc formats.
 
+The next question is, what theme to use? The [Pelican theme gallery](http://pelican-themes-gallery.place.org/) is helpful here. I debated between these themes:
 
+* [Irfan](http://pelican-themes-gallery.place.org/irfan/)
+* [Tuxlite TBS](http://pelican-themes-gallery.place.org/tuxlite_tbs/)
+* [Pelican Mockingbird](http://pelican-themes-gallery.place.org/pelican-mockingbird/)
+* [Built Texts](http://pelican-themes-gallery.place.org/built-texts/)
+
+For now, I'm using [Built Texts](http://pelican-themes-gallery.place.org/built-texts/). It's as easy as setting 
+
+```THEME = /path/to/built-texts``` 
+
+in your pelicanconf.py. 
+
+Also, make sure that you set 
+
+```SITEURL = ''```
+
+in pelicanconf.py. Otherwise, your local preview site will try to load resoureces (css, etc.) from the url of your published site. 
 
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Christian Long'
-SITENAME = u'Blog'
+SITENAME = u"Christian Long's Blog"
 SITEURL = ''
 
 PATH = 'content'
@@ -17,21 +17,74 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
+
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Archives', 'archives.html'),
+        )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'http://twitter.com/christianmlong'),
+         )
 
 DEFAULT_PAGINATION = 10
 DEFAULT_DATE = 'fs'
 
-THEME = '/home/christianmlong/projects/external/pelican-themes/gum'
+_THEME_BASE = '/home/christianmlong/projects/external/pelican-themes/'
+#THEME = _THEME_BASE + 'tuxlite_tbs'
+#THEME = _THEME_BASE + 'pelican-mockingbird'
+THEME = _THEME_BASE + 'built-texts'
 
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+
+DEFAULT_CATEGORY = ('Articles')
+#COPYRIGHT = 'Copyright Christian Long 2014'
+
+STATIC_PATHS = ['images',
+                'pages',
+                'pdfs',
+                #'extra',
+                'extra/robots.txt',
+               ]
+PAGES_PATHS = ['pages']
+
+
+
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = True
+
+#TYPOGRIFY = True
+TYPOGRIFY = False
+
+SLUGIFY_SOURCE = 'title'
+
+ #
+#
+
+# path-specific metadata
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
