91d1ceb3d2b6f99f7698138a235339dcf72b5a3f
Christian Long <christianzlong@gmail.com>
2015-04-01 19:24:16 -0500

    Utility for posting Gists from the command line
    Uses the github3.py library.

---
+++ utilities/github/post_gist.py
@@ -0,0 +1,61 @@
+from github3 import authorize, login
+from getpass import getpass
+
+try:
+    # Python 2
+    prompt = raw_input
+except NameError:
+    # Python 3
+    prompt = input
+
+CREDENTIALS_FILE = r'C:\Users\Christian Long\.github3_py\credentials'
+
+def main():
+    """
+    Main function to post a gist to GitHub.
+    """
+
+def get_session():
+    """
+    Return a logged-in GitHub session. If no cached authorization is found on
+    the local system, this wlll ask the user for username and password, get an
+    auth token from GitHub, and cache the auth token.
+    """
+    token = id = ''
+    try:
+        with open(CREDENTIALS_FILE, 'r') as fd:
+            token = fd.readline().strip()
+            id = fd.readline().strip()
+    except IOError:
+        (username, password) = get_user_pass()
+        auth = get_auth_token_for_gists(username, password)
+        token - auth.token
+        id - auth.id
+        with open(CREDENTIALS_FILE, 'w') as fd:
+            fd.write(token + '\n')
+            fd.write(id)
+
+    return login(token=token)
+
+def get_auth_token_for_gists(username, password):
+    """
+    Log in to GitHub and get an OAuth token for saving gists.
+    """
+    note = 'githubpy3.py for my utility scripts.'
+    scopes = ['gist']
+    return authorize(username, password, scopes, note)
+
+def get_user_pass():
+    """
+    Get the user's username and password.
+    """
+    username = prompt("Enter GitHub username")
+
+    password = ''
+    while not password:
+        password = getpass('Password for {0}: '.format(username))
+
+    return (username, password)
+
+if __name__ == '__main':
+    main()
