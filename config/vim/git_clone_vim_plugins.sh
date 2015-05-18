193eabf15a97556d065f11b2b92aade25cd6055d
Christian Long <christianzlong2@gmail.com>
2015-05-18 15:33:44 -0500

    Add snipmate

--- config/vim/git_clone_vim_plugins.sh
+++ config/vim/git_clone_vim_plugins.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -u
-set -e
 
 git clone git://github.com/altercation/vim-colors-solarized
 git clone git://github.com/tpope/vim-commentary
@@ -16,3 +14,11 @@ git clone git@github.com:kien/ctrlp.vim.git
 # git clone git@github.com:jeetsukumaran/vim-buffergator.git
 
 git clone git@github.com:bronson/vim-visual-star-search.git
+
+
+
+
+git clone https://github.com/tomtom/tlib_vim.git
+git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+git clone https://github.com/garbas/vim-snipmate.git
+git clone https://github.com/honza/vim-snippets.git
