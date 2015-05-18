bdec6535ea66e57f49093c1dd7b35bdce99895ae
Christian Long <christianzlong@gmail.com>
2015-05-18 17:18:14 -0500

    Clean up whitespace

--- python/setup.py
+++ python/setup.py
@@ -4,17 +4,18 @@
 """
 
 from setuptools import setup, find_packages
-setup(
-    name = "Common",
-    version = "4.0.0",
-    packages = find_packages(),
 
-    install_requires = ['pyodbc>=3',
-                       ],
+setup(
+    name="Common",
+    version="4.0.0",
+    packages=find_packages(),
+
+    install_requires=['pyodbc>=3',
+                      ],
 
     # metadata
-    author = "Christian Long",
-    author_email = "christianzlong@gmail.com",
-    description = "Common framework for applications",
+    author="Christian Long",
+    author_email="christianzlong@gmail.com",
+    description="Common framework for applications",
     url='http://christianlong.com',
 )
