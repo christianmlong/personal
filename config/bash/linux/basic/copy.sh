e37299423bb7aaa8c37556ecb52bf70b6f26bd14
Christian Long <christianzlong2@gmail.com>
2016-07-05 10:47:07 -0500

    Use different .gitconfig

--- config/bash/linux/basic/copy.sh
+++ config/bash/linux/basic/copy.sh
@@ -2,4 +2,4 @@
 
 # Copy files in to place
 cp ~/projects/public-personal/config/bash/linux/.bash_aliases ~/.bash_aliases
-cp ~/projects/public-personal/config/git/.gitconfig ~/.gitconfig
+cp ~/projects/public-personal/config/git/cisco/.gitconfig ~/.gitconfig
