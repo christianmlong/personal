bfa0c2de0aa8156fbfeadf442751fe5760f83c4a
Christian Long <christianzlong2@gmail.com>
2016-07-05 10:28:15 -0500

    Add a script to copy files instead of symlinking them

---
+++ config/bash/linux/basic/copy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Copy files in to place
+cp ~/projects/public-personal/config/bash/linux/.bash_aliases ~/.bash_aliases
+cp ~/projects/public-personal/config/git/.gitconfig ~/.gitconfig
