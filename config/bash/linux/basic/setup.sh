f6637601f6d340df7fffc546174782be0397dab9
Christian Long <christianzlong2@gmail.com>
2016-12-09 21:11:52 -0600

    Use a different .gitconfig file

--- config/bash/linux/basic/setup.sh
+++ config/bash/linux/basic/setup.sh
@@ -2,4 +2,4 @@
 
 # Setup symlinks to dotfiles
 ln -s ~/projects/public-personal/config/bash/linux/.bash_aliases ~/.bash_aliases
-ln -s ~/projects/public-personal/config/git/.gitconfig ~/.gitconfig
+ln -s ~/projects/public-personal/config/git/cisco/.gitconfig ~/.gitconfig
