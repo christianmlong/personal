6e90a4d2c6dd37c239084ce99fe1b5b6f0cdc0da
Christian Long <christianzlong2@gmail.com>
2016-12-09 21:32:35 -0600

    Switch to basic .bash_aliases file

--- config/bash/linux/basic/setup.sh
+++ config/bash/linux/basic/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Setup symlinks to dotfiles
-ln -s ~/projects/public-personal/config/bash/linux/.bash_aliases ~/.bash_aliases
+ln -s ~/projects/public-personal/config/bash/linux/basic/.bash_aliases ~/.bash_aliases
 ln -s ~/projects/public-personal/config/git/cisco/.gitconfig ~/.gitconfig
