4632a4720f035cd1d8a46bda04e7f6ac76ee494a
Christian Long <christianzlong2@gmail.com>
2016-12-09 22:16:00 -0600

    Add vim to basic setup

--- config/bash/linux/basic/setup.sh
+++ config/bash/linux/basic/setup.sh
@@ -3,3 +3,5 @@
 # Setup symlinks to dotfiles
 ln -s ~/projects/public-personal/config/bash/linux/basic/.bash_aliases ~/.bash_aliases
 ln -s ~/projects/public-personal/config/git/cisco/.gitconfig ~/.gitconfig
+ln -s ~/projects/public-personal/config/vim/.vimrc_basic ~/.vimrc
+ln -s ~/projects/public-personal/config/vim/.vimrc_common ~/.vimrc_common
