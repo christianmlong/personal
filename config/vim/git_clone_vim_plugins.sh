e0f8151881dc363f3816ecdfaff60634fa44da45
Christian Long <chlong2@cisco.com>
2016-12-15 18:59:28 -0600

    vi: comment out some unused plugins

--- config/vim/git_clone_vim_plugins.sh
+++ config/vim/git_clone_vim_plugins.sh
@@ -14,11 +14,16 @@ git clone git@github.com:vim-airline/vim-airline.git
 git clone git@github.com:vim-airline/vim-airline-themes.git
 
 git clone git@github.com:ctrlpvim/ctrlp.vim.git
-# git clone git@github.com:jeetsukumaran/vim-buffergator.git
 
 git clone git@github.com:bronson/vim-visual-star-search.git
 
+git clone git://github.com/AndrewRadev/linediff.vim.git
 
+# Ansible file support (YAML, Jinja2, hosts file)
+git clone git@github.com:pearofducks/ansible-vim.git
+
+
+# Not currently in use
 
 # vim-snipmate and its requirements
 # git clone https://github.com/tomtom/tlib_vim.git
@@ -27,15 +32,11 @@ git clone git@github.com:bronson/vim-visual-star-search.git
 # git clone https://github.com/honza/vim-snippets.git
 
 # UltiSnips and its requirements
-git clone https://github.com/sirver/ultisnips
-git clone https://github.com/honza/vim-snippets.git
+# git clone https://github.com/sirver/ultisnips
+# git clone https://github.com/honza/vim-snippets.git
 
 
 # git clone https://github.com/sjl/gundo.vim.git
 # git clone git@github.com:KabbAmine/zeavim.vim.git
 
-git clone git://github.com/AndrewRadev/linediff.vim.git
-
-# Ansible file support (YAML, Jinja2, hosts file)
-git clone git@github.com:pearofducks/ansible-vim.git
-
+# git clone git@github.com:jeetsukumaran/vim-buffergator.git
