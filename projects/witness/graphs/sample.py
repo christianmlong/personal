83ccc200af429fc0a33e363222320fbfc674a667
Christian Long <chlong2@cisco.com>
2017-08-22 23:30:22 -0500

    Copy in the Dijkstra function for customization

--- projects/witness/graphs/sample.py
+++ projects/witness/graphs/sample.py
@@ -1,3 +1,7 @@
+from collections import deque
+from heapq import heappush, heappop
+from itertools import count
+
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -19,7 +23,8 @@ def run_it():
     """
     g = nx.read_adjlist('puzzle1.graph')
 
-    nx.set_node_attributes(g, 'absences', 0)
+    nx.set_node_attributes(g, 'ZZZ', 0)
+    print(nx.get_node_attributes(g, 'ZZZ'))
     nx.set_edge_attributes(g, 'absences', 0)
 
 
@@ -117,7 +122,6 @@ def single_source_dijkstra_for_graph_with_weighted_nodes_and_edges(G, source, ta
         # parameter) gets captured here in this nested function (closure).
         edge_weight = edge_data.get(weight, 1)
 
-        import pdb; pdb.set_trace()
         # We only care about the weight of the destination node, not the source
         # node. We already paid the cost of getting to the source node, on the
         # last iteration.
@@ -126,7 +130,8 @@ def single_source_dijkstra_for_graph_with_weighted_nodes_and_edges(G, source, ta
         return edge_weight + destination_node_weight
 
     paths = {source: [source]}  # dictionary of paths
-    return nx.algorithms.shortest_paths.weighted._dijkstra(
+    import pdb; pdb.set_trace()
+    return _dijkstra(
         G,
         source,
         get_weight_for_edge_and_node,
@@ -134,6 +139,92 @@ def single_source_dijkstra_for_graph_with_weighted_nodes_and_edges(G, source, ta
         cutoff=cutoff,
         target=target,
     )
+
+def _dijkstra(G, source, get_weight, pred=None, paths=None, cutoff=None,
+              target=None):
+    """Implementation of Dijkstra's algorithm
+
+    Parameters
+    ----------
+    G : NetworkX graph
+
+    source : node label
+       Starting node for path
+
+    get_weight: function
+        Function for getting edge weight
+
+    pred: list, optional(default=None)
+        List of predecessors of a node
+
+    paths: dict, optional (default=None)
+        Path from the source to a target node.
+
+    target : node label, optional
+       Ending node for path
+
+    cutoff : integer or float, optional
+       Depth to stop the search. Only paths of length <= cutoff are returned.
+
+    Returns
+    -------
+    distance,path : dictionaries
+       Returns a tuple of two dictionaries keyed by node.
+       The first dictionary stores distance from the source.
+       The second stores the path from the source to that node.
+
+    pred,distance : dictionaries
+       Returns two dictionaries representing a list of predecessors
+       of a node and the distance to each node.
+
+    distance : dictionary
+       Dictionary of shortest lengths keyed by target.
+    """
+    G_succ = G.succ if G.is_directed() else G.adj
+
+    push = heappush
+    pop = heappop
+    dist = {}  # dictionary of final distances
+    seen = {source: 0}
+    c = count()
+    fringe = []  # use heapq with (distance,label) tuples
+    push(fringe, (0, next(c), source))
+    while fringe:
+        (d, _, v) = pop(fringe)
+        if v in dist:
+            continue  # already searched this node.
+        dist[v] = d
+        if v == target:
+            break
+
+        for u, e in G_succ[v].items():
+            cost = get_weight(v, u, e)
+            if cost is None:
+                continue
+            vu_dist = dist[v] + get_weight(v, u, e)
+            if cutoff is not None:
+                if vu_dist > cutoff:
+                    continue
+            if u in dist:
+                if vu_dist < dist[u]:
+                    raise ValueError('Contradictory paths found:',
+                                     'negative weights?')
+            elif u not in seen or vu_dist < seen[u]:
+                seen[u] = vu_dist
+                push(fringe, (vu_dist, next(c), u))
+                if paths is not None:
+                    paths[u] = paths[v] + [u]
+                if pred is not None:
+                    pred[u] = [v]
+            elif vu_dist == seen[u]:
+                if pred is not None:
+                    pred[u].append(v)
+
+    if paths is not None:
+        return (dist, paths)
+    if pred is not None:
+        return (pred, dist)
+    return dist
 
 def parse_arguments():
     """
