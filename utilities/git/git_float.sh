244b6582140b529641576b10828667d1ed8de4e9
Christian Long <christianzlong2@gmail.com>
2018-09-26 09:26:20 -0500

    Handle BRANCHES as an array

--- utilities/git/git_float.sh
+++ utilities/git/git_float.sh
@@ -24,34 +24,25 @@ if [ $# -ne 0 ]; then
     fi
 fi
 
-if [ $# -eq 0 ]; then
-    BRANCHES=$(git branch --no-merge)
-else
-    # Here we are taking all of the arguments from the
-    # command line. We used to use "$@", and handle them
-    # as an array, but for parallelism with the other branch
-    # of this if statement, I'm handling them as one string
-    # now.
-    # Reference
-    # http://stackoverflow.com/questions/3008695/what-is-the-difference-between-and-in-bash
-    BRANCHES="$*"
-fi
+# Alternate way; handling BRANCHES as a string instead of as an array.
+# if [ $# -eq 0 ]; then
+#     BRANCHES=$(git branch --no-merge)
+# else
+#     # http://stackoverflow.com/questions/3008695/what-is-the-difference-between-and-in-bash
+#     BRANCHES="$*"
+# fi
 
 # Here's how to handle BRANCHES as an array instead of as a
-# whitespace-delimited string. However, you would have to change things like
-# this
+# whitespace-delimited string. Use the array like this:
 #    git merge --no-ff --no-edit "$BRANCHES"
-# so that they worked on the whole list of branches, not just
-# the first element.
-#
-# declare -a BRANCHES
-# if [ $# -eq 0 ]; then
-#     # Make an array
-#     read -r -a BRANCHES <<< "$(git branch --no-merge)"
-# else
-#     # Read the arguments as an array
-#     BRANCHES=("$@")
-# fi
+declare -a BRANCHES
+if [ $# -eq 0 ]; then
+    # Make an array
+    read -r -a BRANCHES <<< "$(git branch --no-merge)"
+else
+    # Read the arguments as an array
+    BRANCHES=("$@")
+fi
 
 if [ "$CURRENT_BRANCH" == "$INTEGRATION_BRANCH" ]; then
     echo "You are on branch ${CURRENT_BRANCH}. Switch to a feature branch before running"
@@ -73,7 +64,7 @@ git branch -f "$INTEGRATION_BRANCH" "$GIT_FLOAT_BASE_BRANCH"
 # Here we're iterating over a space-delimited string or a newline-delimited
 # string Since IFS is at its default value of (space, tab, newline), this
 # works. The alternative would be to make BRANCHES be an array variable.
-for i in $BRANCHES
+for i in "${BRANCHES[@]}"
 do
     git co "$i"
     git rebase "$GIT_FLOAT_BASE_BRANCH"
@@ -82,14 +73,14 @@ done
 git co "$INTEGRATION_BRANCH"
 
 if [ "$DO_MERGE" = "true" ]; then
-    for i in $BRANCHES
+    for i in "${BRANCHES[@]}"
     do
         # Print out some octopi
         printf '🐙  '
     done
     printf '\n'
 
-    git merge --no-ff --no-edit $BRANCHES
+    git merge --no-ff --no-edit "${BRANCHES[@]}"
 fi
 
 git co "$CURRENT_BRANCH"
